4819665bf70cb1ed6a03ec209b61cae068875915
HARADA Shinya <harada.shinya.0613@gmail.com>
2022-08-20 16:25:03 +0900

    もっと速く押してください！に変更

--- jsPsych/js/plugins/jspsych-categorize-image.js
+++ jsPsych/js/plugins/jspsych-categorize-image.js
@@ -80,7 +80,7 @@ jsPsych.plugins['categorize-image'] = (function() {
       timeout_message: {
         type: jsPsych.plugins.parameterType.STRING,
         pretty_name: 'Timeout message',
-        default: "<p>もう少し速く押してください.</p>",
+        default: "<p>もっと速く押してください！.</p>",
         description: 'The message displayed on a timeout non-response.'
       },
       stimulus_duration: {
